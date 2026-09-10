c396d0bfeacf156807d1638119ecb7254c28be77
Parth Agrawal <parthag1415@gmail.com>
2026-09-10 20:11:50 +0530

    Refactor database engine initialization
    Updated engine creation to use DATABASE_URL.

--- backend/database.py
+++ backend/database.py
@@ -3,9 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "postgresql://user:password@host/dbname"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
