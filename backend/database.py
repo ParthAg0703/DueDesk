f3e1dc7507631fa282691aa37164e2dfea96a1b9
Parth Agrawal <parthag1415@gmail.com>
2026-09-10 20:12:54 +0530

    Fix database URL variable in database.py

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "postgresql://user:password@host/dbname"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
