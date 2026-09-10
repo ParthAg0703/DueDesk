1e1bbbfc3ff2a9bbd4c954c7e140c8705e2f5d87
Parth Agrawal <parthag1415@gmail.com>
2026-09-10 20:17:06 +0530

    Fix indentation in get_db function
    Reformatted the get_db function for consistent indentation.

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://user:password@host/dbname"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
